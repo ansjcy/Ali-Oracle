e6b4db761882f466ea6c227278d844c1748c96d9
ansjcy <168762116@qq.com>
2016-10-03 16:29:43 -0700

    solve by getting two connections

--- select10.js
+++ select10.js
@@ -6,60 +6,82 @@ var outputFileName = "/Users/anakin/Desktop/ali/save.json";
 var rowCount = 0;
 var queryData = {};
 
+/*
+queries:
+// "select distinct QQ_ID from GATHER_QQ_NUMS",
+// "select QQ_ID, NICK from GATHER_QQ_NUMS order by QQ_ID",
+// "select QQ_ID, NICK, IMPTIME from GATHER_QQ_NUMS order by QQ_ID, IMPTIME",
+// "select * from user_tab_columns where Table_Name = :did",
+//"select table_name from user_tables",
+*/
 
 
 oracledb.getConnection({
         user: dbConfig.user,
         password: dbConfig.password,
         connectString: dbConfig.connectString
-    })
-    .then(function(connection) {
-        return connection.execute(
-                "select QQ_ID from GATHER_QQ_NUMS order by QQ_ID, IMPTIME", [], // no bind variables
-                {
-                    resultSet: true,
-                    outFormat: oracledb.OBJECT
-                } // return a result set.  Default is false
-            )
-            .then(function(result) {
+    },
+    function(err, connection) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        connection.execute(
+            "select table_name from user_tables",
+            [], // no bind variables
+            {
+                resultSet: true,
+                outFormat: oracledb.OBJECT
+            }, // return a result set.  Default is false
+            function(err, result) {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
                 console.log("****** this is 'result' : ******");
                 console.log(result);
-                // console.log("****** this is 'metaData' : ******");
-                // console.log(result.resultSet.metaData);
                 console.log("****** start reading lines : ******");
                 fetchOneRowFromRS(connection, result.resultSet);
-            })
-            .catch(function(err) {
-                console.error(err.message);
-                doRelease(connection);
-                return;
-            })
-    })
-    .then(function(connection){
-        console.log("here in the second exe");
-        return connection.execute(
-                "select NICK from GATHER_QQ_NUMS order by QQ_ID, IMPTIME", [], // no bind variables
-                {
-                    resultSet: true,
-                    outFormat: oracledb.OBJECT
-                } // return a result set.  Default is false
-            )
-            .then(function(result) {
-                console.log("****** this is 'result' : ******");
-                console.log(result);
-                // console.log("****** this is 'metaData' : ******");
-                // console.log(result.resultSet.metaData);
-                console.log("****** start reading lines : ******");
-                fetchOneRowFromRS(connection, result.resultSet);
-            })
-            .catch(function(err) {
-                console.error(err.message);
-                doRelease(connection);
-                return;
-            })
+            });
     });
 
-
+oracledb.getConnection({
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
+    },
+    function(err, connection) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        connection.execute(
+            // "select distinct QQ_ID from GATHER_QQ_NUMS",
+            // "select QQ_ID, NICK from GATHER_QQ_NUMS order by QQ_ID",
+            // "select QQ_ID, NICK, IMPTIME from GATHER_QQ_NUMS order by QQ_ID, IMPTIME",
+            "select NICK from GATHER_QQ_NUMS order by QQ_ID, IMPTIME",
+            //  "select * from user_tab_columns where Table_Name = :did",
+            //"select table_name from user_tables",
+            [], // no bind variables
+            {
+                resultSet: true,
+                outFormat: oracledb.OBJECT
+            }, // return a result set.  Default is false
+            function(err, result) {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                console.log("****** this is 'result' : ******");
+                console.log(result);
+                // console.log("****** this is 'metaData' : ******");
+                // console.log(result.resultSet.metaData);
+                console.log("****** start reading lines : ******");
+                fetchOneRowFromRS(connection, result.resultSet);
+            });
+    });
 
 
 
